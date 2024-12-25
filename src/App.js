9c93e4612c0dc91f531c1dc2212ca0b2e28e73a5
krishna <krishnakoturu526@gmail.com>
2024-12-25 19:34:14 +0530

    redux first commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+import { Provider, useDispatch, useSelector } from "react-redux";
+
+const counterSlice = createSlice({
+  name: "krish26",
+  initialState: {
+    value: 13,
+  },
+  reducers: {
+    increment: (state, action) => {
+      state.value = state.value + action.payload;
+    },
+  },
+});
+
+console.log("ok");
+
+const { increment } = counterSlice.actions;
+
+const store = configureStore({
+  reducer: {
+    krish26: counterSlice.reducer,
+  },
+});
+
+const Krishna = () => {
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.krish26.value);
+  console.log(count);
+
+  const addig_15 = () => {
+    dispatch(increment(15));
+  };
+
+  return (
+    <div>
+      <h6>the value is: {count}</h6>
+      <h4>Krishna koturu</h4>
+      <button onClick={addig_15}>Add</button>
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <Krishna />
+    </Provider>
+  );
+}
